81fc8d9336815e62461a08e529ebb28c60252500
Lera <valmarfina@gmail.com>
2021-09-21 23:35:53 +0300

    added realization of dividing elements by the sum of neighboring elements 2)added realization of functions of matrix rotation counter-clockwise by 90-180-270 degrees were added

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
 
+public class Main {
   //функция заполняет массив случайными числами от -N до N
-  public static void randomFill(int[][] myMass, int N) {
+  public static void randomFill(double[][] myMass, int N) {
     Random rnd = new Random();
     for (int i = 0; i < myMass.length; i++) {
       for (int j = 0; j < myMass[i].length; j++) {
@@ -15,80 +15,116 @@ public class Main {
     }
   }
 
-  //функция вывод массива
-  public static void printMass(int[][] myMass) {
-    //System.out.print(Arrays.deepToString(myMass));
-    Arrays.stream(myMass).map(Arrays::toString).forEach(System.out::println);
-    System.out.println();
+  //запись в файл
+  public static void printToFile(double[][] myMass, FileWriter writer) throws Exception {
+    for (double[] elements : myMass) {
+      writer.write(Arrays.toString(elements) + '\n');
+    }
+    writer.write('\n');
   }
 
   //поворот матрицы на 90 градусов против часов стрелки
-  public static int[][] turn90Mass(int[][] myMass) {
-    if (myMass.length == 0) return myMass;
+  public static void turn90Mass(double[][] myMass) {
+    if (myMass.length == 0) return;
 
     int rows = myMass.length, cols = myMass[0].length;
-    int[][] result = new int[cols][rows];
-
+    double[][] temp = new double[cols][rows];
     for (int row = 0; row < cols; row++) {
       for (int col = 0; col < rows; col++) {
-        result[row][col] = myMass[col][rows - row - 1];
+        temp[row][col] = myMass[col][rows - row - 1];
       }
     }
+    for (int i = 0; i < myMass.length; i++) {
+      System.arraycopy(temp[i], 0, myMass[i], 0, myMass.length);
+    }
+  }
+
+  //поворот матрицы на 180 градусов против часов стрелки
+  public static void turn180Mass(double[][] myMass) {
+    if (myMass == null || myMass.length == 0) {
+      return;
+    }
+    if (myMass.length % 2 == 1) {
+      for (int j = 0; j < myMass.length / 2; j++) {
+        double temp = myMass[myMass.length / 2][j];
+        myMass[myMass.length / 2][j] = myMass[myMass.length / 2][myMass.length - j - 1];
+        myMass[myMass.length / 2][myMass.length - j - 1] = temp;
+      }
+    }
+
+    for (int i = 0; i < myMass.length / 2; i++) {
+      for (int j = 0; j < myMass.length; j++) {
+        double temp = myMass[i][j];
+        myMass[i][j] = myMass[myMass.length - i - 1][myMass.length - j - 1];
+        myMass[myMass.length - i - 1][myMass.length - j - 1] = temp;
+      }
+    }
+  }
+
+  //поворот матрицы на 270 градусов против часов стрелки
+  public static void turn270Mass(double[][] myMass) {
+    turn180Mass(myMass);
+    turn90Mass(myMass);
+  }
+
+  //деление элементов на сумму соседних
+  public static void division(double[][] myMass) {
+    double[][] temp = new double[myMass.length + 2][myMass.length + 2];
+    for (int i = 0; i < myMass.length; i++) {
+      System.arraycopy(myMass[i], 0, temp[i + 1], 1, myMass.length);
+    }
+    double sum = 0;
+    for (int i = 1; i < temp.length - 1; i++) {
+      for (int j = 1; j < temp.length - 1; j++) {
+        sum = temp[i - 1][j] + temp[i][j + 1] + temp[i + 1][j] + temp[i][j - 1];
+        if (sum == 0) {
+          throw new ArithmeticException();
+        } else {
+          myMass[i - 1][j - 1] = roundNum(temp[i][j] / sum);
+          sum = 0;
+        }
+      }
+    }
+  }
+
+  public static double roundNum(double value) {
+    double scale = Math.pow(10, 2);
+    double result = Math.ceil(value * scale);
+    result /= scale;
     return result;
   }
 
-  //запись в файл(NOT DONE)
-  public static void write(String filename, int[] x) throws IOException {
-    BufferedWriter outputWriter = null;
-    outputWriter = new BufferedWriter(new FileWriter(filename));
-    outputWriter.flush();
-    outputWriter.close();
-  }
-
-  //запись в файл
-  public static void printToFileFirst(int[][] myMass, FileWriter writer) throws Exception {
-    for (int[] elements : myMass) {
-      writer.write(Arrays.toString(elements) + '\n');
-    }
-    writer.write('\n');
-  }
-
-  //запись в файл
-  private static void printToFile(int[][] turn90Mass, FileWriter writer) throws IOException {
-    for (int[] elements : turn90Mass) {
-      writer.write(Arrays.toString(elements) + '\n');
-    }
-    writer.write('\n');
-  }
-
-
   public static void main(String[] args) {
     try {
-
-      //считываем размерность матрицы из файла file.txt
       Scanner scanner = new Scanner(new File("D:\\study\\POLYTEC\\ява\\лабы\\labs\\textFiles_matrixes\\src\\file.txt"));
       int N = scanner.nextInt();
+      if (N <= 0) throw new ArithmeticException();
 
-      //запись в файл NOT DONE
-      FileWriter writer = new FileWriter("file_out.txt");
+      double[][] myMass = new double[N][N];
 
-      //массив который заполним радомно
-      int[][] myMass = new int[N][N];
-      //функция заполняет массив случайными числами от -N до N
+      //заполняем массив случайными числами от -N до N
       randomFill(myMass, N);
 
+      FileWriter writer = new FileWriter("file_out.txt");
+
       writer.write("полученная матрица из элементов " + N + " на " + N + '\n');
-      //функция вывод массива
-      printToFileFirst(myMass, writer);
+      printToFile(myMass, writer);
 
       writer.write("поворот матрицы на 90 градусов против часовой стрелки" + '\n');
-      printToFile(turn90Mass(myMass), writer);
+      turn90Mass(myMass);
+      printToFile(myMass, writer);
 
       writer.write("поворот матрицы на 180 градусов против часовой стрелки" + '\n');
-      printToFile(turn90Mass(turn90Mass(myMass)), writer);
+      turn180Mass(myMass);
+      printToFile(myMass, writer);
 
       writer.write("поворот матрицы на 270 градусов против часовой стрелки" + '\n');
-      printToFile(turn90Mass(turn90Mass(turn90Mass(myMass))), writer);
+      turn270Mass(myMass);
+      printToFile(myMass, writer);
+
+      writer.write("деление элементов матрицы на сумму соседних" + '\n');
+      division(myMass);
+      printToFile(myMass, writer);
 
       writer.close();
 
@@ -96,6 +132,4 @@ public class Main {
       e.printStackTrace();
     }
   }
-
-
 }
